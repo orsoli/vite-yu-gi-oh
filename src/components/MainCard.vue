53b79bba319dda829ad5423d2870e7d4321e036a
orsoli <fljcoli@gmail.com>
2024-09-18 17:36:34 +0200

    added props in MainCard

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,11 +1,25 @@
 <script>
+
 export default {
-    name: "",
     data() {
         return {
-
         };
     },
+    props: {
+        cardName: {
+            type: String,
+            required: true,
+        },
+        cardImage: {
+            type: String,
+            required: true,
+        },
+        cardArchetype: {
+            type: String,
+            required: true,
+        },
+    },
+
     methods: {
 
     }
@@ -13,9 +27,21 @@ export default {
 </script>
 
 <template>
-    <div class="">
-
-    </div>
+    <main>
+        <div class="card" style="width: 18rem;">
+            <img :src="cardImage" class="card-img-top" :alt="cardName">
+            <div class="card-body">
+                <h5 class="card-title">{{ cardName }}</h5>
+                <p class="card-text">{{ cardArchetype }}</p>
+            </div>
+        </div>
+    </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+main {
+    h5 {
+        color: white;
+    }
+}
+</style>
--- src/components/MainCardsList.vue
+++ src/components/MainCardsList.vue
@@ -6,7 +6,7 @@ import MainCard from './MainCard.vue';
 import axios from 'axios';
 
 // Import store
-import { store } from "../store.js";
+import { store } from "../store";
 
 export default {
     data() {
@@ -21,22 +21,21 @@ export default {
     methods: {
         getCardsList() {
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=39&offset=0').then((response) => {
-                console.log(response.data.data) // Test printing in console
+                // console.log(response.data.data) // Test printing in console
                 this.store.cardsList = response.data.data
+                console.log(this.store.cardsList[0].card_images[0].image_url) // Test print in console
             })
         }
     },
     mounted() {
         this.getCardsList()
-        console.log(store.cardsList) // Test print in console
     }
 };
 </script>
 
 <template>
-    <div class="">
-
-    </div>
+    <MainCard v-for="card in store.cardsList" :key="card.id" :cardName="card.name"
+        :cardImage="card.card_images[0].image_url" :cardArchetype="card.archetype" />
 </template>
 
 <style scoped></style>
